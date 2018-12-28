0381087468ffd600135b30704f6bccbac63669ee
keeper <keeper_louis@sina.com>
2018-12-28 13:47:28 +0800

    add test by keeper at  2018/12/28

---
+++ TLMBProject/KEEPER.K3.TLMB.CUSTOMER.ServicePlugIn/Audit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEPER.K3.TLMB.CUSTOMER.ServicePlugIn
+{
+    public class Audit
+    {
+    }
+}
