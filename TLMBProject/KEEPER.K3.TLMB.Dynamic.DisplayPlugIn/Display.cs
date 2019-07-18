c13657f237cccd3c7d6e9bcea70e79dcbfb93b32
keeper <keeper_louis@sina.com>
2019-07-18 10:03:58 +0800

    add "陈列地图webapi接口" by keeper at 20190718

--- TLMBProject/KEEPER.K3.TLMB.Dynamic.DisplayPlugIn/Display.cs
+++ TLMBProject/KEEPER.K3.TLMB.Dynamic.DisplayPlugIn/Display.cs
@@ -1,4 +1,5 @@
-﻿using Kingdee.BOS.Core.DynamicForm.PlugIn;
+﻿using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
 using Kingdee.BOS.Core.Permission.Objects;
 using Kingdee.BOS.JSON;
 using Kingdee.BOS.ServiceHelper;
@@ -28,7 +29,7 @@ namespace KEEPER.K3.TLMB.Dynamic.DisplayPlugIn
                 BusinessInfo = formMetaData.BusinessInfo,
                 IsLookUp = true,//是否基础资料权限
                 bzIsolateOrgIds = OrgIds,
-                OrgId = this.Context.CurrentOrganizationInfo.ID,
+                //OrgId = this.Context.CurrentOrganizationInfo.ID,
                 ParentFormId = "PAEZ_DisPlayShow"//货架陈列展示FORMID
             };
 
@@ -42,6 +43,32 @@ namespace KEEPER.K3.TLMB.Dynamic.DisplayPlugIn
             };
 
             Kingdee.BOS.Orm.DataEntity.DynamicObject[] stockObjects = BusinessDataServiceHelper.Load(this.View.Context, formMetaData.BusinessInfo.GetDynamicObjectType(), queryParameter);
+            if (stockObjects.Count()>0)
+            {
+                string strSql = string.Format(@"/*dialect*/SELECT FLINKOBJECT FROM T_SEC_USER WHERE FUSERID = {0}", this.View.Context.UserId);
+                long personID = DBUtils.ExecuteScalar<long>(this.View.Context, strSql, 0, null);
+                if (personID>0)
+                {
+                    string searchSql = string.Format(@"SELECT COUNT(1) FROM USER_TABLES WHERE TABLE_NAME = 'TABLE{0}'", personID);
+                    int num = DBUtils.ExecuteScalar<int>(this.View.Context, searchSql, 0, null);
+                    if (num > 0)
+                    {
+                        string dropSql = string.Format(@"/*dialect*/drop table TABLE{0}", personID);
+                        DBUtils.Execute(this.View.Context, dropSql);
+                    }
+                    string createSql = string.Format(@"/*dialect*/create table TABLE{0}
+(
+  DEPTID NUMBER(10) not null,
+  DEPTNAME   NVARCHAR2(255) not null
+)",personID);
+                    DBUtils.Execute(this.View.Context, createSql);
+                    object[] stockId =  (from c in stockObjects select c[0]).ToArray();
+                    string ids = string.Join(",", stockId);
+                    string insertSql = string.Format(@"/*dialect*/INSERT INTO TABLE{0} SELECT DEPT.FDEPTID,DEPTL.FNAME FROM T_BD_DEPARTMENT DEPT INNER JOIN T_BD_DEPARTMENT_L DEPTL ON DEPT.FDEPTID = DEPTL.FDEPTID WHERE DEPT.FDEPTID IN ({1})", personID, ids);
+                    DBUtils.Execute(this.View.Context, insertSql);
+                }
+                
+            }
             JSONObject webobj = new JSONObject();
             webobj["source"] = @"http://221.180.255.112:9000/taoli/action2/delivery_deliveryDisplayWeb.action";
             webobj["height"] = 545;
---
+++ TLMBProject/KEEPER.K3.TLMB.GetDisplayRoute.WebApiPlugIn/GetDisplayRouteData.cs
@@ -0,0 +1,52 @@
+﻿using Kingdee.BOS.WebApi.ServicesStub;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingdee.BOS.ServiceFacade.KDServiceFx;
+using Kingdee.BOS;
+using Kingdee.BOS.ServiceHelper;
+using Kingdee.BOS.Core.Permission.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Orm.DataEntity;
+using KEEPER.K3.TLMB.Core;
+
+namespace KEEPER.K3.TLMB.GetDisplayRoute.WebApiPlugIn
+{
+    [Description("获取陈列地图数据权限范围")]
+    public class GetDisplayRouteData : AbstractWebApiBusinessService
+    {
+        public GetDisplayRouteData(KDServiceContext context) : base(context)
+        {
+        }
+        public Context Ctx
+        {
+            get
+            {
+                return this.KDContext.Session.AppContext;
+            }
+        }
+
+        public string getData(string parameterJson)
+        {
+            JObject jo = (JObject)JsonConvert.DeserializeObject(parameterJson);
+            long personId = Convert.ToInt64(jo.Root["personId"].ToString());
+            string strSql = string.Format(@"/*dialect*/SELECT DEPTID,DEPTNAME FROM TABLE{0}", personId);
+            DynamicObjectCollection doo = DBUtils.ExecuteDynamicObject(Ctx, strSql);
+            List<Dept> deptList = new List<Dept>();
+            foreach (DynamicObject item in doo)
+            {
+                Dept dept = new Dept();
+                dept.deptId = Convert.ToInt64(item["DEPTID"]);
+                dept.deptname = Convert.ToString(item["DEPTNAME"]);
+                deptList.Add(dept);
+            }
+            string result = JsonConvert.SerializeObject(deptList);
+            return result;
+        }
+    }
+}
