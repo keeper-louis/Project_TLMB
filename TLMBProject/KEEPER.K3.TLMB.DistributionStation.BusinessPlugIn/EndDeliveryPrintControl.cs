86e85184ac7528af4efe02f0ebf31d2049053012
keeper <keeper_louis@sina.com>
2019-09-10 14:07:18 +0800

    add "分销站结算明细表结单后才可打印"

---
+++ TLMBProject/KEEPER.K3.TLMB.DistributionStation.BusinessPlugIn/EndDeliveryPrintControl.cs
@@ -0,0 +1,22 @@
+﻿using Kingdee.BOS.Core.Bill.PlugIn;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+
+namespace KEEPER.K3.TLMB.DistributionStation.BusinessPlugIn
+{
+    [Description("分销站结算明细表结单后才可以打印")]
+    public class EndDeliveryPrintControl:AbstractBillPlugIn
+    {
+        
+        public override void BarItemClick(BarItemClickEventArgs e)
+        {
+            base.BarItemClick(e);
+
+        }
+    }
+}
