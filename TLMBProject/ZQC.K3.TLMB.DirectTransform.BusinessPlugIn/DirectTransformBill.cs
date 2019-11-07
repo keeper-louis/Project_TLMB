07ce4a5c09ee1b6f8c86fb380c06922e69f145f8
keeper <keeper_louis@sina.com>
2019-11-07 13:30:54 +0800

    add "直接调拨单服务端保存校验" add "直接调拨单表单插件"  by ZQC at 2019/11/7

---
+++ TLMBProject/ZQC.K3.TLMB.DirectTransform.BusinessPlugIn/DirectTransformBill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZQC.K3.TLMB.DirectTransform.BusinessPlugIn
+{
+    public class DirectTransformBill : 
+    {
+
+    }
+}
