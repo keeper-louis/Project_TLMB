cda533400c48f638048f4135426af5f0aab473fb
keeper <keeper_louis@sina.com>
2019-11-15 12:52:24 +0800

    add "销售出库单两插件，销售退货单保存插件"

---
+++ TLMBProject/ZQC.K3.TLMB.SalBack.BusinessPlugin/SalBackStateChange.cs
@@ -0,0 +1,73 @@
+﻿using Kingdee.BOS.App;
+using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Contracts;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Core.Metadata;
+using Kingdee.BOS.Orm.DataEntity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZQC.K3.TLMB.SalBack.BusinessPlugin
+{
+    [Description("销售退货单库存状态改变")]
+    public class SalBackStateChange : AbstractOperationServicePlugIn
+    {
+        //库存状态对象
+        DynamicObject BasicObject;
+        //物料编码必须加载
+        public override void OnPreparePropertys(PreparePropertysEventArgs e)
+        {
+            base.OnPreparePropertys(e);
+            e.FieldKeys.Add("FMATERIALID");
+        }
+        //操作初始化时确认操作执行参数
+        public override void BeginOperationTransaction(BeginOperationTransactionArgs e)
+        {
+            base.BeginOperationTransaction(e);
+            string strSql = string.Format(@"/*dialect*/select A.FSTOCKSTATUSID from t_BD_StockStatus A inner join t_Bd_Stockstatus_l B on A.FSTOCKSTATUSID = B.FSTOCKSTATUSID where B.FNAME = '不参与核算'");
+            long result = DBUtils.ExecuteScalar<long>(base.Context, strSql, -1, null);
+            DynamicObject[] entitys = e.DataEntitys;
+            if (result > 0)
+            {
+                IMetaDataService metaService = ServiceHelper.GetService<IMetaDataService>();//元数据服务
+                IViewService view = ServiceHelper.GetService<IViewService>();//界面服务
+                FormMetadata Meta = metaService.Load(base.Context, "BD_StockStatus") as FormMetadata;//获取基础资料元数据
+                BasicObject = view.LoadSingle(base.Context, result, Meta.BusinessInfo.GetDynamicObjectType());//不参与核算的库存状态ID
+            }
+            foreach (DynamicObject entity in entitys)
+            {//FSTOCKSTATUSID库存状态  编码  07040000 07040004
+                if (entity != null)
+                {
+
+                    DynamicObjectCollection entityInfo = entity["SAL_RETURNSTOCKENTRY"] as DynamicObjectCollection;
+                    for (int i = 0; i < entityInfo.Count; i++)
+                    {
+                        DynamicObject material = entityInfo[i]["MATERIALID"] as DynamicObject;
+
+                        if (material["Number"].Equals("07040000") || material["Number"].Equals("07040004"))
+                        {
+                            if (BasicObject != null)
+                            {
+                                //string updateStr = string.Format(@"/*dialect*/Update T_STK_MISCELLANEOUSENTRY set FSTOCKSTATUSID = {0} where fentryid = {1}", result, Convert.ToInt64(entityInfo[i]["Id"]));
+                                //DBUtils.Execute(base.Context, updateStr);
+                                entityInfo[i]["StockstatusId"] = BasicObject;
+                                entityInfo[i]["StockstatusId_Id"] = result;
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+        //操作执行前（事务内）事件，在数据检查完毕，启动事务之后触发。
+
+
+    }
+}
---
+++ TLMBProject/ZQC.K3.TLMB.SalOutStock.BussinessPlugIn/customerBoxAmount.cs
@@ -0,0 +1,30 @@
+﻿using Kingdee.BOS.Core.Bill.PlugIn;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingdee.BOS.Core.Bill.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+
+namespace ZQC.K3.TLMB.SalOutStock.BussinessPlugIn
+{
+    [Description("销售出库单子客户计算箱套数量")]
+   public class customerBoxAmount : AbstractBillPlugIn
+    {
+        //根据组织确定其是否是分公司销售，保存前根据物料判断 并且新增行计算箱套数量添加到分录中
+        public override void BeforeSave(BeforeSaveEventArgs e)
+        {
+            base.BeforeSave(e);
+            //1.获取单据头使用组织
+            DynamicObject useOrg = this.Model.GetValue("FUSEORGID") as DynamicObject;
+            long useOrgId = Convert.ToInt64(useOrg["Id"]);
+            //通过sql查询该组织下对应所有子客户
+            string sql = string.Format(@"select t1.FcustId 
+                                           from t_bd_customer t1
+                                           where t1.FPRIMARYGROUP=105322  and FUSEORGID = {0}",useOrgId); 
+
+        }
+    }
+}
